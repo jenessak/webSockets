6925fea9198cb2ecf054848a9d4e0eeddcd3d63b
Jenessa Kaumans <jenessa@rocketmade.com>
2017-04-26 08:52:15 -0600

    fourth commit

--- public/app.js
+++ public/app.js
@@ -118,7 +118,7 @@ angular.module('mimicMe').service('SockService', [function () {
 
 
   function onopen (name) {
-    var socket = new WebSocket('ws:https://mimicme2.herokuapp.com/');
+    var socket = new WebSocket('wss://mimicme2.herokuapp.com/');
     socket.onopen = function () {
       var obj = {};
       obj.action = "introduce";
